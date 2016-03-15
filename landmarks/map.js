0ecbf4b06ee4f8ab634085aa3fb32a2cc0bcf2da
Anna <anna.stroe@tufts.edu>
2016-03-14 21:14:20 -0400

    adding icons

--- landmarks/map.js
+++ landmarks/map.js
@@ -1,5 +1,17 @@
 
+function createMap() {
+	var myLoc = new google.maps.LatLng(-71, 42);
+	var myOptions = {
+			zoom: 10,
+			center: myLoc,
+			mapTypeId:google.maps.MapTypeId.ROADMAP
 
+		};
+
+	map = new google.maps.Map(document.getElementById("map"), myOptions);
+
+};
+/*
 var myLatitude = 0;
 var myLongitude = 0;
 var map;
@@ -10,7 +22,6 @@ function getMyLocation() {
 		navigator.geolocation.getCurrentPosition(function(position) {
 			myLatitude = position.coords.latitude;
 			myLongitude = position.coords.longitude;
-
 			initialize();
 		});
 	}
@@ -33,12 +44,11 @@ function initialize(){
 }
 
 function createMap() {
-
 	var myLoc = new google.maps.LatLng(myLatitude, myLongitude);
 	var myOptions = {
 			zoom: 10,
 			center: myLoc,
-			/*mapTypeId: google.maps.mapTypeId.ROADMAP */
+			mapTypeId:google.maps.MapTypeId.ROADMAP
 
 		};
 
@@ -47,40 +57,62 @@ function createMap() {
 
 }
 
-// create icons
-
-
 
 function parse() {
+	var my_icon = {
+	url: "my_icon_cat.png",
+	scaledSize: new google.maps.Size(30, 30)
+	};
 
+	// create icons
+	var student_icon = {
+		url: "student_icon_dog.png",
+		scaledSize: new google.maps.Size(30, 30)
+
+	};
+
+	var landmark_icon = {
+		url: "food_bowl",
+		scaledSize: new google.maps.Size(30, 30)
+	};
+
+	// get/parse data 
 	if (request.readyState == 4 && request.status == 200) {
 		raw = request.responseText;
 		data = JSON.parse(raw);
 		
 		// for loop to loop through people
-		for (int i = 0; i < data.people.length; i++) {
+		for (var i = 0; i < data.people.length; i++) {
 			// create a marker for each person (google API)
 			marker = new google.maps.Marker({
-				position: LatLng,
+				position: google.maps.LatLng(data.people[i].Latitude,data.people[i].Longitude),
 				title: data.people[i].login,
-				map: map
-				// ANYTHING ELSE????????? 
-				})
-
-			marker.setPosition(new google.maps.LatLng(lat, lng));
+				map: map,
+				icon: student_icon
+				});
 		}
+		
 		// for loop to loop through landmarks
-		for (int i = 0; i < data.landmarks.length; i++) {
+		for (var i = 0; i < data.landmarks.length; i++) {
 			// create a marker for each landmark
 			marker = new google.maps.Marker({
-				position: LatLng,
-				title: //TITLE??????????,
-				map: map
-				})
-
-			//map: map,
+				position: google.maps.LatLng(data.landmarks[i].Latitude,data.landmarks[i].Longitude),
+				title: data.landmarks[i].properties.Location_Name,
+				map: map,
+				icon: landmark_icon
+				});
 			}
+
+
+		marker = new google.maps.Marker({
+			position: google.maps.LatLng(myLatitude, myLongitude),
+			title: "MEOW! IT'S ME!",
+			map: map,
+			icon: my_icon
+		});
+
 		console.log(data);
+		
 		
 	}
 	else if (request.readyState != 4 && request.status != 200) {
@@ -88,6 +120,7 @@ function parse() {
 	}
 }
 
+// open info window
 
 //  find closest landmark
 //  haversine formula
